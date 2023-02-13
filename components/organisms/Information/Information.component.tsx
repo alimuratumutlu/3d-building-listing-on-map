27a32db89e6caa8511fe2910f0c7b53edb63cb09
Murat Umutlu <murat@muum.dev>
2023-02-13 04:02:23 +0300

    statistics component

--- components/organisms/Information/Information.component.tsx
+++ components/organisms/Information/Information.component.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styles from "styles/Information.module.css";
+
+// Components
+import { Typography } from "components";
+
+// Hooks
+import useCustomizer from "hooks/useCustomizer";
+
+// Constants
+import { TOTAL_HEIGHT_LABEL } from "@constants/Titles";
 
 export default function Information() {
-  return <div>Information.component</div>;
+  const { options, numberOfFloors, floorHeight } = useCustomizer();
+
+  return (
+    <div className={styles.container}>
+      {options.map((option) => {
+        return (
+          <Typography key={option.label}>
+            {option.label}: {option.value}
+          </Typography>
+        );
+      })}
+      <Typography>
+        {TOTAL_HEIGHT_LABEL}: {numberOfFloors * floorHeight}
+      </Typography>
+    </div>
+  );
 }
