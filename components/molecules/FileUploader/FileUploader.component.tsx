bb9b674ed1c31da8de38541fc2fd0644fb6f41b4
Murat Umutlu <murat@muum.dev>
2023-02-13 03:05:33 +0300

    Geojson uploader

--- components/molecules/FileUploader/FileUploader.component.tsx
+++ components/molecules/FileUploader/FileUploader.component.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Button, Input, Typography } from "components";
 import styles from "styles/FileUploader.module.css";
 
+// Components
+import { Button, Input, Typography } from "components";
+
+// Hooks
+import useGeojson from "hooks/useGeojson";
+
 export default function FileUploader() {
+  const { uploadFile } = useGeojson();
+
   return (
     <div className={styles.container}>
       <Typography variant="h6">File Uploader</Typography>
-      <Input type="file" />
+      <Input type="file" onChange={uploadFile} />
       <Button label="Upload GeoJSON" />
     </div>
   );
---
+++ hooks/useGeojson.tsx
@@ -0,0 +1,40 @@
+import { useMemo, useState } from "react";
+import { GeoJsonLayer } from "@deck.gl/layers";
+
+export default function useGeojson() {
+  const [geojson, setGeojson] = useState("");
+
+  const layers = useMemo(() => {
+    return [
+      new GeoJsonLayer({
+        id: "geojson-layer",
+        data: geojson,
+        stroked: true,
+        filled: true,
+        extruded: true,
+        lineWidthMinPixels: 2,
+        getFillColor: [255, 0, 0, 255],
+        getLineColor: [0, 0, 0],
+        getRadius: 100,
+        getLineWidth: 1,
+        getElevation: 30,
+      }),
+    ];
+  }, [geojson]);
+
+  const uploadFile = (event: any) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        const data = JSON.parse(reader.result as string);
+        setGeojson(data);
+      }
+    };
+
+    reader.readAsText(file);
+  };
+
+  return { geojson, layers, uploadFile };
+}
