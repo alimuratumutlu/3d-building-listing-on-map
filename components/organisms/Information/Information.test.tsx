13495f55aadd7f2fcff1e4eaad571c3a6a804ae1
Murat Umutlu <murat@muum.dev>
2023-02-13 04:05:42 +0300

    information / statistics component test

---
+++ components/organisms/Information/Information.test.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { render } from "@testing-library/react";
+import Information from "./Information.component";
+import { TOTAL_HEIGHT_LABEL } from "@constants/Titles";
+
+describe("Information component", () => {
+  it("renders the information", () => {
+    const { getByText } = render(<Information />);
+    expect(getByText(TOTAL_HEIGHT_LABEL)).toBeDefined();
+  });
+});
